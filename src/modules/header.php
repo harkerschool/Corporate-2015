6198f43e2745c7e4a0574d02971f04a1766c61e2
Joe Banks <joeb@harker.org>
2015-05-15 10:59:15 -0700

    Insert viewport meta tag into head

--- src/modules/header.php
+++ src/modules/header.php
@@ -3,7 +3,10 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
 <title><?php echo $title; ?></title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="css/main.css">
 <style type="text/css">
 </style>
