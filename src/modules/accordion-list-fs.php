932825e8aa76c56188bdf17aa494b806f53488f1
Joe Banks <joeb@harker.org>
2015-05-27 14:53:50 -0700

    Style Finalsite accordion.

---
+++ src/modules/accordion-list-fs.php
@@ -0,0 +1,191 @@
+<section class="green-bg fsElement fsAccordion fsPanelIconBefore small-block-grid-1 medium-block-grid-2 large-block-grid-4" id="fsEl_136">
+    <header>
+        <h2 class="fsElementTitle">Accordion</h2>
+    </header>
+    <!-- ul.accordion -->
+    <div class="fsElementContent">
+        <!-- li.accordion-navigation  -->
+        <section class="fsElement fsPanel fsStateOpen" id="fsEl_137">
+            <!-- a -->
+            <header>
+                <h2 class="fsElementTitle">Panel 1</h2>
+            </header>
+            <!-- div.panel -->
+            <div class="fsElementContent" style="display: block;">
+                <div class="fsElement fsContent" id="fsEl_138">
+                    <div class="fsElementContent">
+                        <ul class="accordion-list">
+                            <li>
+                                Theater <i class="fa fa-check"></i> <i class="fa fa-child"></i>
+                            </li>
+                            <li>
+                                Jazz
+                                <i class="fa fa-check"></i>
+                                <i class="fa fa-child"></i>
+                            </li>
+                            <li>
+                                Dance
+                                <i class="fa fa-check"></i>
+                                <i class="fa fa-child"></i>
+                            </li>
+                            <li>Tech</li>
+                        </ul>
+                        <p>
+                            Panel 6. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p>
+                            <a href="http://www.harker.org/page.cfm?p=2793" class="button">Learn more</a>
+                        </p>
+                    </div>
+                </div>
+                <section class="fsElement fsAccordion fsPanelIconBefore" id="fsEl_157">
+                    <div class="fsElementContent">
+                        <section class="fsElement fsPanel fsStateOpen" id="fsEl_158">
+                            <header>
+                                <h2 class="fsElementTitle">New Panel</h2>
+                            </header>
+                            <div class="fsElementContent" style="display: block;">
+                                <div class="fsElement fsContent" id="fsEl_159">
+                                    <div class="fsElementContent">
+                                        <p>Hello World</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="fsElement fsPanel" id="fsEl_160">
+                            <header>
+                                <h2 class="fsElementTitle">New Panel</h2>
+                            </header>
+                            <div class="fsElementContent" style="display: none;">
+                                <div class="fsElement fsContent" id="fsEl_161">
+                                    <div class="fsElementContent">Hello World</div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </section>
+            </div>
+        </section>
+        <section class="fsElement fsPanel" id="fsEl_145">
+            <header>
+                <h2 class="fsElementTitle">Panel 2</h2>
+            </header>
+            <div class="fsElementContent" style="display: none;">
+                <div class="fsElement fsContent" id="fsEl_146">
+                    <div class="fsElementContent">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fsElement fsPanel" id="fsEl_147">
+            <header>
+                <h2 class="fsElementTitle">Panel 3</h2>
+            </header>
+            <div class="fsElementContent" style="display: none;">
+                <div class="fsElement fsContent" id="fsEl_148">
+                    <div class="fsElementContent">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fsElement fsPanel" id="fsEl_149">
+            <header>
+                <h2 class="fsElementTitle">Panel 4</h2>
+            </header>
+            <div class="fsElementContent" style="display: none;">
+                <div class="fsElement fsContent" id="fsEl_150">
+                    <div class="fsElementContent">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fsElement fsPanel" id="fsEl_137">
+            <!-- a -->
+            <header>
+                <h2 class="fsElementTitle">Panel 1</h2>
+            </header>
+            <!-- div.panel -->
+            <div class="fsElementContent" style="display: block;">
+                <div class="fsElement fsContent" id="fsEl_138">
+                    <div class="fsElementContent">
+                        <ul>
+                            <li>
+                                Theater
+                                <i class="fa fa-check"></i>
+                                <i class="fa fa-child"></i>
+                            </li>
+                            <li>
+                                Jazz
+                                <i class="fa fa-check"></i>
+                                <i class="fa fa-child"></i>
+                            </li>
+                            <li>
+                                Dance
+                                <i class="fa fa-check"></i>
+                                <i class="fa fa-child"></i>
+                            </li>
+                            <li>Tech</li>
+                        </ul>
+                        <p>
+                            Panel 6. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p>
+                            <a href="http://www.harker.org/page.cfm?p=2793" class="button">Learn more</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fsElement fsPanel" id="fsEl_145">
+            <header>
+                <h2 class="fsElementTitle">Panel 2</h2>
+            </header>
+            <div class="fsElementContent" style="display: none;">
+                <div class="fsElement fsContent" id="fsEl_146">
+                    <div class="fsElementContent">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fsElement fsPanel" id="fsEl_147">
+            <header>
+                <h2 class="fsElementTitle">Panel 3</h2>
+            </header>
+            <div class="fsElementContent" style="display: none;">
+                <div class="fsElement fsContent" id="fsEl_148">
+                    <div class="fsElementContent">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fsElement fsPanel" id="fsEl_149">
+            <header>
+                <h2 class="fsElementTitle">Panel 4</h2>
+            </header>
+            <div class="fsElementContent" style="display: none;">
+                <div class="fsElement fsContent" id="fsEl_150">
+                    <div class="fsElementContent">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</section>
--- src/modules/accordion-list.php
+++ src/modules/accordion-list.php
@@ -1,77 +1,108 @@
-<div class="section">
+<div class="section purple-bg">
     <div class="row">
         <div class="column">
             <h2>Accordion List</h2>
-            <ul class="accordion small-block-grid-1 medium-block-grid-2 large-block-grid-4" data-accordion="programs">
-              <li class="accordion-navigation">
-                <a href="#panel1a">English</a>
-                <div id="panel1a" class="content">
-                  Panel 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </div>
-              </li>
-              <li class="accordion-navigation">
-                <a href="#panel2a">Foreign Language</a>
-                <div id="panel2a" class="content">
-                  <p>Panel 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                  <ul>
-                    <li>Spanish</li>
-                    <li>French</li>
-                    <li>Chinese</li>
-                  </ul>
-                </div>
-              </li>
-              <li class="accordion-navigation">
-                <a href="#panel3a">Mathematics</a>
-                <div id="panel3a" class="content">
-                  Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </div>
-              </li>
-              <li class="accordion-navigation">
-                <a href="#science">Science</a>
-                <div id="science" class="content">
-                  <ul class="accordion"data-accordion="science-courses">
-                    <li class="accordion-navigation">
-                      <a href="#science-a">Biology <i class="fa fa-check"></i><i class="fa fa-child"></i></a>
-                      <div id="science-a" class="content">
-                        Panel 4a. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      </div>
-                    </li>
-                    <li class="accordion-navigation">
-                      <a href="#science-b">Physics <i class="fa fa-check"></i><i class="fa fa-child"></i></a>
-                      <div id="science-b" class="content">
-                        Panel 4a. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      </div>
-                    </li>
-                    <li class="accordion-navigation">
-                      <a href="#science-c">Chemistry <i class="fa fa-check"></i></a>
-                      <div id="science-c" class="content">
-                        Panel 4a. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      </div>
-                    </li>
-                  </ul>
-                  <p>Panel 4. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-              </li>
-              <li class="accordion-navigation">
-                <a href="#tech">Computer Science</a>
-                <div id="tech" class="content">
-                  Panel 5. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </div>
-              </li>
-              <li class="accordion-navigation">
-                <a href="http://www.harker.org/page.cfm?p=2793" class="accordion-direct-link"><i class="fa fa-link"></i></a>
-                <a href="#pa">Performing Arts</a>
-                <div id="pa" class="content">
-                  <ul>
-                    <li>Theater <i class="fa fa-check"></i> <i class="fa fa-child"></i></li>
-                    <li>Jazz <i class="fa fa-check"></i> <i class="fa fa-child"></i></li>
-                    <li>Dance <i class="fa fa-check"></i> <i class="fa fa-child"></i></li>
-                    <li>Tech</li>
-                  </ul>
-                  <p>Panel 6. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                  <p><a href="http://www.harker.org/page.cfm?p=2793" class="button">Learn more</a></p>
-                </div>
-              </li>
+            <ul class="accordion accordion-invert small-block-grid-1 medium-block-grid-2 large-block-grid-4" data-accordion="programs">
+                <li class="accordion-navigation">
+                    <a href="#panel1a">English</a>
+                    <div id="panel1a" class="content">
+                        Panel 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                </li>
+                <li class="accordion-navigation">
+                    <a href="#panel2a">Foreign Language</a>
+                    <div id="panel2a" class="content">
+                        <p>
+                            Panel 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <ul>
+                            <li>Spanish</li>
+                            <li>French</li>
+                            <li>Chinese</li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="accordion-navigation">
+                    <a href="#panel3a">Mathematics</a>
+                    <div id="panel3a" class="content">
+                        Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                </li>
+                <li class="accordion-navigation">
+                    <a href="#science">Science</a>
+                    <div id="science" class="content">
+                        <ul class="accordion"data-accordion="science-courses">
+                            <li class="accordion-navigation">
+                                <a href="#science-a">
+                                    Biology <i class="fa fa-check"></i> <i class="fa fa-child"></i>
+                                </a>
+                                <div id="science-a" class="content">
+                                    Panel 4a. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </div>
+                            </li>
+                            <li class="accordion-navigation">
+                                <a href="#science-b">
+                                    Physics
+                                    <i class="fa fa-check"></i>
+                                    <i class="fa fa-child"></i>
+                                </a>
+                                <div id="science-b" class="content">
+                                    Panel 4a. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </div>
+                            </li>
+                            <li class="accordion-navigation">
+                                <a href="#science-c">
+                                    Chemistry
+                                    <i class="fa fa-check"></i>
+                                </a>
+                                <div id="science-c" class="content">
+                                    Panel 4a. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </div>
+                            </li>
+                        </ul>
+                        <p>
+                            Panel 4. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                    </div>
+                </li>
+                <li class="accordion-navigation">
+                    <a href="#tech">Computer Science</a>
+                    <div id="tech" class="content">
+                        Panel 5. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                </li>
+                <li class="accordion-navigation">
+                    <a href="http://www.harker.org/page.cfm?p=2793" class="accordion-direct-link">
+                        <i class="fa fa-link"></i>
+                    </a>
+                    <a href="#pa">Performing Arts</a>
+                    <div id="pa" class="content">
+                        <ul>
+                            <li>
+                                Theater
+                                <i class="fa fa-check"></i>
+                                <i class="fa fa-child"></i>
+                            </li>
+                            <li>
+                                Jazz
+                                <i class="fa fa-check"></i>
+                                <i class="fa fa-child"></i>
+                            </li>
+                            <li>
+                                Dance
+                                <i class="fa fa-check"></i>
+                                <i class="fa fa-child"></i>
+                            </li>
+                            <li>Tech</li>
+                        </ul>
+                        <p>
+                            Panel 6. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p>
+                            <a href="http://www.harker.org/page.cfm?p=2793" class="button">Learn more</a>
+                        </p>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
