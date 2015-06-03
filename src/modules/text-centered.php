d84fbfeb8cd0cb76397a3d1367253d0dff880a4a
Joe Banks <joeb@harker.org>
2015-06-02 18:02:39 -0700

    Add new module and images.

---
+++ src/modules/text-centered.php
@@ -0,0 +1,10 @@
+<div class="section">
+    <div class="row">
+        <div class="column medium-12 medium-centered">
+            <h2 id="bookmark-support">Support & Services</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            </p>
+        </div>
+    </div>
+</div>
