2d9f8d288df342d1bcfef9432bf4fb974d7bd36e
Joe Banks <joeb@harker.org>
2015-06-04 13:13:25 -0700

    Tweak layout from Nicks suggestions.

--- src/modules/intro.php
+++ src/modules/intro.php
@@ -1,7 +1,8 @@
 <div class="section">
     <div class="row">
-        <div class="column medium-18 medium-centered">
-            <p class="intro" id="bookmark-intro" data-bookmark-label="Intro">
+        <div class="column">
+            <p class="h1" id="bookmark-intro" data-bookmark-label="Intro">Lower School</p>
+            <p class="intro">
                 In a supportive and nurturing environment, our exceptionally qualified faculty build tailored learning programs and opportunities for our youngest students. Specialists begin teaching in kindergarten, and students take pride and pleasure in their accomplishments in an environment of honesty and personal accountability.
             </p>
         </div>
