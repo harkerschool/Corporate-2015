99affab5661450c6c463c43191adb2908308d7b5
Joe Banks <joeb@harker.org>
2015-10-01 17:12:30 -0700

    Uninstall hologram docs. Autoplay stats slider.

--- src/js/vendor/harker.js
+++ src/js/vendor/harker.js
@@ -133,39 +133,41 @@ hkr.slider = {
         $('.feature-slider').slick({
             autoplay: true 
         });
-        this.statsSlider();
+        this.statsSlider(); 
     },
     statsSlider: function() {
         var counters = [],
             $countEl = $('.count'),
             $slider = $('.stats-slider').slick({
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true,
-                        dots: true
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 640,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
                     }
-                },
-                {
-                    breakpoint: 640,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
+                ]
         });
 
         if ( $countEl.length === 0 ) {
