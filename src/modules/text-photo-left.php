fd3904e8f684a11cef934b9d517c386217999ed8
Joe Banks <joeb@harker.org>
2016-10-10 10:36:12 -0700

    Add panel-outline class

--- src/modules/text-photo-left.php
+++ src/modules/text-photo-left.php
@@ -4,17 +4,19 @@
             <img src="http://dummyimage.com/600x600/333/ddd.gif&text=placeholder+image" alt="placeholder+image">
         </div>
         <div class="fade-in right-to-left column column-expand medium-13" data-equalizer-watch>
-            <h2>Text + Photo Module</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </p>
-            <p><a href="#" class="button button-outline">Learn more about this topic</a></p>
+            <div class="panel panel-outline-invert">
+                <h2>Text + Photo Module</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                </p>
+                <p><a href="#" class="button button-outline">Learn more about this topic</a></p>
+            </div>
         </div>
     </div>
 </div>
